14f12c661697709529911550f5389963228c7aff
Andry-Ks <127143251+Andry-Ks@users.noreply.github.com>
2024-09-16 14:37:53 +0300

    fix tests

--- cypress/e2e/homePage.cy.js
+++ cypress/e2e/homePage.cy.js
@@ -12,15 +12,28 @@ describe("Головна сторінка сайту", () => {
         cy.get('.sc-kiTBBF.eWyMGQ').click(); //X
 
         cy.contains('a', 'Про магазин').click();
+        cy.get('.sc-klxZGT.dbcffv').should('be.visible'); //Текстовий блок
         cy.contains('a', 'Доставка, оплата і повернення').click();
+        cy.get('.sc-klxZGT.dbcffv').should('be.visible'); //Текстовий блок
         cy.contains('a', 'Контакти і зворотний зв’язок').click();
+        cy.get('.sc-bPNAxY').should('be.visible'); //Текстовий блок
+        cy.get('input[placeholder="Email"]').should('be.visible');
+        cy.get('input[placeholder="Ім’я"]').should('be.visible');
+        cy.get('input[placeholder="+380"]').should('be.visible');
+        cy.get('input[placeholder="Введіть повний номер замовлення"]').should('be.visible');
+        cy.get('textarea[placeholder="Ваше повідомлення"]').should('be.visible');
+        cy.get('input[type="checkbox"]').check();
+        cy.get('.sc-hSWyVn').should('be.visible'); //Відправка форми в розробці
         cy.contains('a', 'Book Club').click();
+        cy.get('.sc-klxZGT').should('be.visible'); //Текстовий блок
         cy.contains('a', 'Політика конфіденційності').click();
+        cy.get('.sc-bYycxU').should('be.visible'); //Текстовий блок
         cy.contains('a', 'Умови користування').click();
+        cy.get('.sc-bYycxU').should('be.visible'); //Текстовий блок
         cy.get('rect[fill="url(#pattern0_6014_4787)"]').click({ multiple: true }); //Лого
 
-        cy.contains('span', '+380 96 123 45 67').should('be.visible');
+        cy.get('.sc-bJdqqZ > :nth-child(1) > span').should('be.visible'); //Телефон
         cy.contains('span', 'bukarka.info@gmail.com').should('be.visible');
-        cy.get('.sc-hVsJot.dRYqOO').should('be.visible'); //Copyright
+        cy.get('.sc-cQXfRj').should('be.visible'); //Copyright
     });
 });
