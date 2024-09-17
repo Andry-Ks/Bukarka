c96c1c25aedf90c628cabdc892a13b29dd438149
Andry-Ks <127143251+Andry-Ks@users.noreply.github.com>
2024-09-17 03:21:10 +0300

    add screenshots and video

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: true, // Включає запис відео
+  screenshotsFolder: 'cypress/screenshots',
+  videosFolder: 'cypress/videos',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
